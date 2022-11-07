46154e877f05dc41ea01b6cc7dcbc6b9b2769d32
VictorLascares <vmlascares@gmail.com>
2022-11-07 15:23:55 -0600

    Agregando componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-  <h1 class="text-red-600">Hola Mundo</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+  </div>
 </template>
