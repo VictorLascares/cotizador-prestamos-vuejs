95d5bf15d94be7f29445023918d809f48660a22d
VictorLascares <vmlascares@gmail.com>
2022-11-08 10:30:15 -0600

    Creando la seccion de resumen de pagos

--- src/App.vue
+++ src/App.vue
@@ -75,5 +75,17 @@ function handleClickIncrease() {
         <option value="24">24 meses</option>
       </select>
     </div>
+
+    <div class="my-5 space-y-3 bg-gray-50 p-5">
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Resumen <span class="text-indigo-600">de pagos</span>
+      </h2>
+
+      <p class="text-xl text-gray-500 text-center font-bold">
+        {{ term }} meses
+      </p>
+      <p class="text-xl text-gray-500 text-center font-bold">total a pagar</p>
+      <p class="text-xl text-gray-500 text-center font-bold">mensuales</p>
+    </div>
   </div>
 </template>
